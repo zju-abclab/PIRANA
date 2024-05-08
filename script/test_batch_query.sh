0190a9375321b07b99f2784acd4b526cc60c533e
jing_yu_li <3170105759@zju.edu.cn>
2024-05-08 23:54:54 +0800

    fix some bug

--- script/test_batch_query.sh
+++ script/test_batch_query.sh
@@ -2,6 +2,7 @@ rm -r batch_query_test_log
 mkdir batch_query_test_log
 cd batch_query_test_log
 
+# Two example for test:
 for ((i = 8 ; i <= 12; i++)) 
 do
     for j in 1 
@@ -12,12 +13,12 @@ do
     done
 done
 
-# for ((i = 8 ; i <= 12; i++)) 
-# do
-#     for j in 1 
-#     do
-#         l=$((2**i))
-# 	    echo "../../bin/pirexamples -b 1 -l $l -n 1048576 -x 256 -c 0 >> batch_query.txt"
-# 	    ../../bin/pirexamples -b 1 -l $l -n 1048576 -x 256 -c 0 >> batch_query.txt
-#     done
-# done
+for ((i = 8 ; i <= 12; i++)) 
+do
+    for j in 1 
+    do
+        l=$((2**i))
+	    echo "../../bin/pirexamples -b 1 -l $l -n 1048576 -x 256 -c 0 >> batch_query.txt"
+	    ../../bin/pirexamples -b 1 -l $l -n 1048576 -x 256 -c 0 >> batch_query.txt
+    done
+done
